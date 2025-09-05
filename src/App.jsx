4a406ae4a53b7fd7a6e697b7c89e86e0355a81c0
Manuel Estrada <estradamanuel15@yahoo.com>
2025-09-05 14:59:42 -0300

    Footer y cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,30 @@ import NavBarTop from "./components/NavBarTop";
 import NavBarBottom from "./components/NavBarBottom";
 import Carousel from "./components/Carousel";
 import ItemListContainer from "./components/ItemListContainer";
-import ProductList from "./components/ProductList"; // nuevo import
+import ProductList from "./components/ProductList";
+import Footer from "./components/Footer";
+import "./App.css"; // 👈 acá pondremos estilos generales
 
 function App() {
   return (
-    <div>
-      <NavBarTop />
-      <NavBarBottom />
-
-      {/* Carrusel debajo de la navbar-bottom */}
-      <div style={{ marginTop: "170px" }}>
-        <Carousel />
+    <>
+      <div className="app-container">
+        <NavBarTop />
+        <NavBarBottom />
+        <div style={{ marginTop: "170px", display: "flex", justifyContent: "center" }}>
+          <Carousel />
+        </div>
+        <ItemListContainer greeting="¡Bienvenido a Genesis Airsoft!" />
+        <div style={{ margin: "40px 20px" }}>
+          <ProductList />
+        </div>
+        
       </div>
-
-      {/* Mensaje de bienvenida */}
-      <ItemListContainer greeting="¡Bienvenido a Genesis Airsoft!" />
-
-      {/* Cards de productos debajo */}
-      <div style={{ margin: "40px 20px" }}>
-        <ProductList />
-      </div>
-    </div>
+     <Footer />
+    </>
   );
 }
+
+
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+// src/components/Footer.jsx
+import React from "react";
+import "./Footer.css";
+import logo from "../assets/LogoGenesis.png"; // ajusta la ruta a tu logo
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      {/* Parte superior con el logo */}
+      <div className="footer-top">
+        <img src={logo} alt="Logo Genesis Airsoft" className="footer-logo" />
+      </div>
+
+      {/* Parte inferior con copyright */}
+      <div className="footer-bottom">
+        <p>Copyright ©️ 2023. Todos los derechos reservados.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
