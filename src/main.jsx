4b51bf48a388fe4e5b34d74fd0de6c9ade8f5f35
Manuel Estrada <estradamanuel15@yahoo.com>
2025-10-13 18:11:14 -0300

    Correcion

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import AuthProvider from "./context/authProvider.jsx";
+import AuthProvider from "./context/AuthProvider.jsx";
 import CartProvider from "./context/CartProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
